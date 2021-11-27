735d151668441fdbd54fb9a093fd0d52a30a5fb5
ElBlasco69 <blascovi@outlook.it>
2021-11-27 13:39:13 +0100

    Rimozione animazione hamburger ma aggiunta di un menù a scomparsa laterale

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ app.get("/", (request, response) => {
     response.send("Ciao mondo");
 });
 
-function myFunction(x) {
-    x.classList.toggle("change");
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
