38681780621a2fdf53dfff260bf1d689b4e44bd7
ElBlasco69 <blascovi@outlook.it>
2021-11-27 11:29:59 +0100

    aggiunta di un menù ad hambuerger e di un po di testo

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const Express = require("express");
 const bodyParser = require("body-parser");
 const app = Express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(1025, () => {});
+
 app.get("/", (request, response) => {
     response.send("Ciao mondo");
 });
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
